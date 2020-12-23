e56fb522a55e17569bb366d4c5bb98b50a557d92
rijiprem <rijir.kmr@gmail.com>
2020-12-23 20:22:57 +0530

    added js link

--- index.php
+++ index.php
@@ -42,8 +42,9 @@
     <div class="carousel-item active">
       <img src="img1.jpg" class="d-block w-100" alt="...">
     </div>
-    <
+    
   </div>
-</div> 
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script> 
 </body>
 </html>
